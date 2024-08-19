0569595ed246642f1c7b6b255be163617aeba840
Gabloctopi18 <arush.sastry@gmail.com>
2024-08-18 18:55:32 -0700

    started a robot.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot.java
@@ -0,0 +1,86 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.lynx.LynxModule;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AngularVelocity;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+
+import java.util.List;
+public class robot {
+    private DcMotor leftFront = null;
+    private DcMotor leftBack = null;
+    private DcMotor rightFront = null;
+    private DcMotor rightBack = null;
+    private DcMotor vertSlidesLeft = null;
+    private DcMotor vertSlidesRight = null;
+    private DcMotor horSlides = null;
+    private DcMotor outtakeArm = null;
+
+    int depositPosition = 100;
+    int pickupPosition = -100;
+    double leftIntakePickupPos = 1;
+    double leftIntakeDropPos = 0;
+    double rightIntakePickupPos = 1;
+    double rightIntakeDropPos = 0;
+    double vertClawClosePos = 1;
+    double vertClawOpenPos = 0;
+    double horClawClosePos = 1;
+    double horClawOpenPos = 0;
+    private LinearOpMode myOpMode;
+
+    public DcMotor leftVertEncoder = null;
+    public DcMotor rightVertEncoder = null;
+    public DcMotor horEncoder = null;
+    public IMU imu;
+
+    private boolean showTelemetry = false;
+
+    public void SimplifiedOdometryRobot(LinearOpMode opmode) {
+        myOpMode = opmode;
+    }
+
+    public void initialize(boolean showTelemetry){
+        leftFront = setupDriveMotor("leftFront", DcMotor.Direction.REVERSE);
+        leftBack = setupDriveMotor("leftBack", DcMotor.Direction.REVERSE);
+        rightFront = setupDriveMotor("rightFront", DcMotorSimple.Direction.FORWARD);
+        rightBack = setupDriveMotor("rightBack", DcMotorSimple.Direction.FORWARD);
+        vertSlidesLeft = setupDriveMotor("vertSlidesLeft", DcMotor.Direction.FORWARD);
+        vertSlidesRight = setupDriveMotor("vertSlidesRight", DcMotor.Direction.REVERSE);
+        horSlides = setupDriveMotor("horSlides", DcMotor.Direction.REVERSE);
+        imu = myOpMode.hardwareMap.get(IMU.class, "imu");
+
+        leftVertEncoder = myOpMode.hardwareMap.get(DcMotor.class, "leftVertEncoder");
+        rightVertEncoder = myOpMode.hardwareMap.get(DcMotor.class, "rightVertEncoder");
+        horEncoder = myOpMode.hardwareMap.get(DcMotor.class, "horEncoder");
+
+        List<LynxModule> allHubs = myOpMode.hardwareMap.getAll(LynxModule.class);
+        for (LynxModule module : allHubs) {
+            module.setBulkCachingMode(LynxModule.BulkCachingMode.AUTO);
+        }
+
+        RevHubOrientationOnRobot orientationOnRobot =
+                new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                        RevHubOrientationOnRobot.UsbFacingDirection.FORWARD);
+        imu.initialize(new IMU.Parameters(orientationOnRobot));
+
+        this.showTelemetry = showTelemetry;
+    }
+
+    private DcMotor setupDriveMotor(String deviceName, DcMotor.Direction direction) {
+        DcMotor aMotor = myOpMode.hardwareMap.get(DcMotor.class, deviceName);
+        aMotor.setDirection(direction);
+        aMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);  // Reset Encoders to zero
+        aMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        aMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);  // Requires motor encoder cables to be hooked up.
+        return aMotor;
+    }
+
+}
